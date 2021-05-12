fd66d7cdebe4584283d78dc5020b476429d0bc4f
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-05-12 09:58:48 -0700

    Initial Commit

---
+++ PathOperations/PathOperationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathOperationsApp.swift
+//  PathOperations
+//
+//  Created by Vadiraj Hippargi on 5/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PathOperationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
